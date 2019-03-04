3dc2e33c900ccc1731671963a397a64f033b7d88
Aravin008 <aravind.madabhavi@gmail.com>
2019-03-04 15:15:56 +0530

    modified docker login method and repo name

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #docker login -u $DOCKER_USER -p $DOCKER_PASS
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+export REPO="aravin007/cicd-buzz"
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t aravin008/cicd-buzz:$TAG .
-docker push aravin008/cicd-buzz
+docker build -f Dockerfile -t $REPO:$TAG .
+docker push $REPO
