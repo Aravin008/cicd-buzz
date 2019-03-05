744f9d87c24524fac7b7a322041dfcd856d2b6dd
Aravin008 <aravind.madabhavi@gmail.com>
2019-03-05 17:55:13 +0530

    updated values

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pwd
-yes | curl -fsSL https://get.docker.com -o get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
 yes | sudo sh get-docker.sh
-yes | sudo docker run -d -p 80:5000 aravin008/cicd-buzz
+sudo systemctl start docker
+sudo docker run -d -p 80:5000 aravin008/cicd-buzz
