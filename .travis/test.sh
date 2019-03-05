53b69e8bd45ffd94cec0da0941217a3ae592df23
Aravin008 <aravind.madabhavi@gmail.com>
2019-03-05 19:07:10 +0530

    trial_4

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,4 +2,7 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 yes | sudo sh get-docker.sh
 sudo systemctl start docker
-sudo docker run -d -p 80:5000 aravin008/cicd-buzz
+sudo docker container stop flask_app
+sudo docker rm flask_app
+sudo docker rmi aravin008/cicd-buzz
+sudo docker run -d --name flask_app -p 80:5000 aravin008/cicd-buzz
