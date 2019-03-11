b9fde296854ed0af935d73f77daae0bd613a7c53
Aravin008 <aravind.madabhavi@gmail.com>
2019-03-11 18:56:38 +0530

    testing code standard tool

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+""" This application host the gerator function
+    as web application.
+    - sample application to test the functionality of
+    various tools associated with python.
+"""
 import os
 from flask import Flask
 from buzz import generator
 
 app = Flask(__name__)
 
-@app.route("/")
+app.route("/")
 def generate_buzz():
+    """ converts generator function to
+        web application using flask.
+    """
     page = '<html><body><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
